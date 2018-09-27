715f52d69711af310370b268b008deae5db18010
Robbert Dekker <Robbert.Dekker2@hva.nl>
2018-09-27 12:48:52 +0200

    Auto stash before merge of "EnemiesSpawningBranch" and "origin/master"

---
+++ KickStart/Assets/Scripts/SpawnBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBehaviour : MonoBehaviour {
+
+    public GameObject Enemy;
+
+	// Use this for initialization
+	void Start () {
+        //spawnEnemy();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            spawnEnemy();
+        }
+	}
+
+    private void spawnEnemy()
+    {
+        GameObject tempEnemy = Instantiate(Enemy, new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10)), Quaternion.identity);
+    }
+}
