82a7b8cac1e17798de7cb5a8e20b0e4e6dcda59f
Huub van de Hoef <Huub_thomas@hotmail.com>
2018-10-01 14:23:58 +0200

    Minor Changes

--- KickStart/Assets/Scripts/Managers/SpawnManager.cs
+++ KickStart/Assets/Scripts/Managers/SpawnManager.cs
@@ -42,7 +42,8 @@ public class SpawnManager : MonoBehaviour {
             //Set right spawn point 
             Vector3 _direction =  new Vector3(Random.Range(-1,1), Random.Range(-1, 1), Random.Range(-1, 1));
             float _distance = Random.Range(-10, 10);
-           // Debug.Log(_direction * _distance);
+           
+            // Debug.Log(_direction * _distance);
             if (_distance < 5)
             {
                 _distance = 5;
@@ -51,6 +52,7 @@ public class SpawnManager : MonoBehaviour {
             {
                 _distance = -5;
             }
+
             _bird.transform.position = _direction * _distance;
             _bird.transform.position = new Vector3(_bird.transform.position.x, Random.Range(0.5f, 2.6f), _bird.transform.position.z);
         }
