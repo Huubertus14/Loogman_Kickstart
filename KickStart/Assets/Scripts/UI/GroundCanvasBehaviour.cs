1135d0e6fa906f5a822aa49c42832496cd26bce3
Huub van de Hoef <Huub_thomas@hotmail.com>
2018-12-19 12:45:57 +0100

    Ground canvas fix

---
+++ KickStart/Assets/Scripts/UI/GroundCanvasBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCanvasBehaviour : MonoBehaviour {
+
+    Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - Camera.main.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Camera.main.transform.position + offset;
+	}
+}
