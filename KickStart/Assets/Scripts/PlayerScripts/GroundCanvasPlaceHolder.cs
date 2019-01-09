bd686d36a590e819703f59bd53538eac87a7ab0e
Huub van de Hoef <Huub_thomas@hotmail.com>
2019-01-09 15:13:00 +0100

    Begin gemaakt ground canvas

---
+++ KickStart/Assets/Scripts/PlayerScripts/GroundCanvasPlaceHolder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCanvasPlaceHolder : MonoBehaviour {
+
+    Vector3 orginLocalPosition;
+
+	// Use this for initialization
+	void Start () {
+        orginLocalPosition = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localPosition = new Vector3(transform.position.x, orginLocalPosition.y, transform.position.z);
+	}
+}
--- KickStart/Assets/Scripts/UI/GroundCanvasBehaviour.cs
+++ KickStart/Assets/Scripts/UI/GroundCanvasBehaviour.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GroundCanvasBehaviour : MonoBehaviour {
-
-    Vector3 offset;
+    
+    public GameObject PlayerGroundCanvasPlace;
+    private Vector3 orginPlace;
 
 	// Use this for initialization
 	void Start () {
-        offset = transform.position - Camera.main.transform.position;
+        orginPlace = PlayerGroundCanvasPlace.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position = Camera.main.transform.position + offset;
+        transform.position = Vector3.Lerp(transform.position,orginPlace, Time.deltaTime * 10);
+        transform.position = new Vector3(transform.position.x, orginPlace.y,transform.position.z);
+        transform.LookAt(Camera.main.transform.position);
+        transform.Rotate(0,180,0);
 	}
 }
